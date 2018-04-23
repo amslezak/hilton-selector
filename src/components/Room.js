9323b9fb1e90ab232d320cf38cdfd2d06f6f7a93
Andy Slezak <andy.slezak@gmail.com>
2018-04-23 18:28:16 -0400

    Flexbox cleanup

--- src/components/Room.js
+++ src/components/Room.js
@@ -33,46 +33,48 @@ export default class Room extends Component {
 
   render() {
     return (
-      <RoomWrapper enabled={this.props.enabled} p={0} m={2}>
-        <Box bg="#E7E7E7" p={3}>
-          <Label color={this.props.enabled ? "black" : "gray3"}>
-            <input
-              checked={this.props.enabled}
-              type="checkbox"
-              onClick={this.onToggleRoom}
-            />
-            Room {this.props.room}
-          </Label>
-        </Box>
-        <Box p={3}>
-          <Text color={this.props.enabled ? "black" : "gray2"} p={1}>
-            Adults (18+)
-          </Text>
-          <Select
-            value={this.props.adult}
-            disabled={!this.props.enabled}
-            my={2}
-            onChange={this.onUpdateAdult}
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-          </Select>
-          <Text color={this.props.enabled ? "black" : "gray2"} p={1}>
-            Children (0-17)
-          </Text>
-          <Select
-            onChange={this.onUpdateChildren}
-            value={this.props.children}
-            disabled={!this.props.enabled}
-            my={2}
-          >
-            <option value="0">0</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-          </Select>
-          <Text />
-        </Box>
-      </RoomWrapper>
+      <Box w={[1, 1 / 2, 1 / 4]}>
+        <RoomWrapper enabled={this.props.enabled} p={0} m={2}>
+          <Box bg="#E7E7E7" p={3}>
+            <Label color={this.props.enabled ? "black" : "gray3"}>
+              <input
+                checked={this.props.enabled}
+                type="checkbox"
+                onClick={this.onToggleRoom}
+              />
+              Room {this.props.room}
+            </Label>
+          </Box>
+          <Box p={3}>
+            <Text color={this.props.enabled ? "black" : "gray2"} p={1}>
+              Adults (18+)
+            </Text>
+            <Select
+              value={this.props.adult}
+              disabled={!this.props.enabled}
+              my={2}
+              onChange={this.onUpdateAdult}
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+            </Select>
+            <Text color={this.props.enabled ? "black" : "gray2"} p={1}>
+              Children (0-17)
+            </Text>
+            <Select
+              onChange={this.onUpdateChildren}
+              value={this.props.children}
+              disabled={!this.props.enabled}
+              my={2}
+            >
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+            </Select>
+            <Text />
+          </Box>
+        </RoomWrapper>
+      </Box>
     )
   }
 }
--- src/containers/RoomContainer.js
+++ src/containers/RoomContainer.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react"
-import { Flex, Box, Button, Container } from "rebass"
+import { Flex, Box, Button } from "rebass"
 
 class RoomContainer extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Flex>{this.props.children}</Flex>
-        <Flex justifyContent="center">
-          <Container>
-            <Box w={1}>
-              <Button>Submit</Button>
-            </Box>
-          </Container>
+      <Flex column alignItems="center" justifyContent="center">
+        <Flex wrap>{this.props.children}</Flex>
+        <Flex>
+          <Box alignSelf="center" justifySelf="center" w={1} p={2}>
+            <Button alignSelf="center" justifySelf="center">
+              Submit
+            </Button>
+          </Box>
         </Flex>
-      </React.Fragment>
+      </Flex>
     )
   }
 }
